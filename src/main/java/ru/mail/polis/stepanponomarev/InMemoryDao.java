bb200f21e3db3046ac15767c68f36f2c2aa909df
stepan-ponomarev <step.ponomarev@gmail.com>
2022-03-07 14:49:00 +0300

    memory segments

--- src/main/java/ru/mail/polis/stepanponomarev/InMemoryDao.java
+++ src/main/java/ru/mail/polis/stepanponomarev/InMemoryDao.java
@@ -1,18 +1,36 @@
 package ru.mail.polis.stepanponomarev;
 
+import jdk.incubator.foreign.MemoryAccess;
+import jdk.incubator.foreign.MemorySegment;
 import ru.mail.polis.Dao;
 import ru.mail.polis.Entry;
 
-import java.nio.ByteBuffer;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
-public class InMemoryDao implements Dao<ByteBuffer, Entry<ByteBuffer>> {
-    private final SortedMap<ByteBuffer, Entry<ByteBuffer>> store = new ConcurrentSkipListMap<>();
+public class InMemoryDao implements Dao<MemorySegment, Entry<MemorySegment>> {
+    private final static Comparator<MemorySegment> memorySegmentComparator = (MemorySegment o1, MemorySegment o2) -> {
+        if (o1 == o2) {
+            return 0;
+        }
+
+        long mismatch = o1.mismatch(o2);
+        if (mismatch == -1) {
+            return 0;
+        }
+
+        byte byteAtOffset1 = MemoryAccess.getByteAtOffset(o1, mismatch);
+        byte byteAtOffset2 = MemoryAccess.getByteAtOffset(o2, mismatch);
+
+        return byteAtOffset1 < byteAtOffset2 ? -1 : 1;
+    };
+
+    private final SortedMap<MemorySegment, Entry<MemorySegment>> store = new ConcurrentSkipListMap<>(memorySegmentComparator);
 
     @Override
-    public Iterator<Entry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) {
+    public Iterator<Entry<MemorySegment>> get(MemorySegment from, MemorySegment to) {
         if (from == null && to == null) {
             return store.values().iterator();
         }
@@ -29,7 +47,7 @@ public class InMemoryDao implements Dao<ByteBuffer, Entry<ByteBuffer>> {
     }
 
     @Override
-    public void upsert(Entry<ByteBuffer> entry) {
+    public void upsert(Entry<MemorySegment> entry) {
         if (entry == null) {
             throw new NullPointerException("Entry can't be null");
         }
