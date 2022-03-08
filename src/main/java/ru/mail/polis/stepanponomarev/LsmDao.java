5bc9421fa8a26ad57e5e2c15762e7f5bc0e3a391
stepan-ponomarev <step.ponomarev@gmail.com>
2022-03-09 01:52:54 +0300

    changed

--- src/main/java/ru/mail/polis/stepanponomarev/LsmDao.java
+++ src/main/java/ru/mail/polis/stepanponomarev/LsmDao.java
@@ -17,18 +17,18 @@ public class LsmDao implements Dao<ByteBuffer, Entry<ByteBuffer>> {
     private static final String SSTABLE_DIR_NAME = "table";
 
     private final Path path;
-    private final List<SSTable> ssTables;
+    private final List<SSTable> SSTables;
     private final SortedMap<ByteBuffer, Entry<ByteBuffer>> memTable = new ConcurrentSkipListMap<>();
 
     public LsmDao(Path bathPath) throws IOException {
         path = bathPath;
-        ssTables = createSSTables(path);
+        SSTables = createSSTables(path);
     }
 
     @Override
     public Iterator<Entry<ByteBuffer>> get(ByteBuffer from, ByteBuffer to) throws IOException {
         List<Iterator<Entry<ByteBuffer>>> iterators = new ArrayList<>();
-        for (SSTable table : ssTables) {
+        for (SSTable table : SSTables) {
             iterators.add(table.get(from, to));
         }
 
@@ -52,13 +52,13 @@ public class LsmDao implements Dao<ByteBuffer, Entry<ByteBuffer>> {
 
     @Override
     public void flush() throws IOException {
-        final Path ssTableDir = path.resolve(SSTABLE_DIR_NAME + ssTables.size());
+        final Path ssTableDir = path.resolve(SSTABLE_DIR_NAME + SSTables.size());
         Files.createDirectory(ssTableDir);
 
         final SSTable ssTable = new SSTable(ssTableDir);
         ssTable.flush(memTable.values().iterator());
 
-        ssTables.add(ssTable);
+        SSTables.add(ssTable);
         memTable.clear();
     }
 
