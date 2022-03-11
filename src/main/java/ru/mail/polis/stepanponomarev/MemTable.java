4911bc25dea846a26409e5866c43c1ba3ea0dd5b
stepan-ponomarev <step.ponomarev@gmail.com>
2022-03-11 05:29:53 +0300

    works

--- src/main/java/ru/mail/polis/stepanponomarev/MemTable.java
+++ src/main/java/ru/mail/polis/stepanponomarev/MemTable.java
@@ -2,62 +2,35 @@ package ru.mail.polis.stepanponomarev;
 
 import ru.mail.polis.Entry;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
 import java.util.Iterator;
 import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class MemTable {
-    private final SortedMap<OSXMemorySegment, Entry<OSXMemorySegment>> memTable;
-    private SizeCounter sizeCounter;
-
-    private static class SizeCounter {
-        private transient LongAdder adder;
-        private static final VarHandle ADDER;
-
-        static {
-            try {
-                ADDER = MethodHandles.lookup().findVarHandle(SizeCounter.class, "adder", LongAdder.class);
-            } catch (Exception e) {
-                throw new ExceptionInInitializerError(e);
-            }
-        }
-
-        public long size() {
-            LongAdder a;
-            long c;
-            do {
-            } while ((a = adder) == null &&
-                    !ADDER.compareAndSet(this, null, a = new LongAdder()));
-            return ((c = a.sum()) <= 0L) ? 0L : c; // ignore transient negatives
-        }
-
-        private void addSize(long c) {
-            LongAdder a;
-            do {
-            } while ((a = adder) == null &&
-                    !ADDER.compareAndSet(this, null, a = new LongAdder()));
-            a.add(c);
-        }
-    }
-
-    public MemTable() {
-        sizeCounter = new SizeCounter();
-        memTable = new ConcurrentSkipListMap<>();
-    }
+    private final Lock lock = new ReentrantLock();
+    private final SortedMap<OSXMemorySegment, Entry<OSXMemorySegment>> memTable = new ConcurrentSkipListMap<>();
+    private final AtomicLong sizeBytes = new AtomicLong(0);
 
     public Entry<OSXMemorySegment> put(OSXMemorySegment key, Entry<OSXMemorySegment> value) {
         final Entry<OSXMemorySegment> oldElement = memTable.get(key);
-        final Entry<OSXMemorySegment> entry = memTable.put(key, value);
 
-        final long addedSize = key.size()
-                - (oldElement == null ? 0 : oldElement.value().size())
-                + (value == null ? 0 : value.value().size());
-        sizeCounter.addSize(addedSize);
+        try {
+            lock.lock();
+            final Entry<OSXMemorySegment> entry = memTable.put(key, value);
 
-        return entry;
+            final long addedByteSize = key.size()
+                    - (oldElement == null ? 0 : oldElement.value().size())
+                    + (value == null ? 0 : value.value().size());
+
+            sizeBytes.addAndGet(addedByteSize);
+
+            return entry;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Iterator<Entry<OSXMemorySegment>> get() {
@@ -65,16 +38,21 @@ public final class MemTable {
     }
 
     public long sizeBytes() {
-        return this.sizeCounter.size();
+        return sizeBytes.get();
     }
 
     public int size() {
-        return this.memTable.size();
+        return memTable.size();
     }
 
     public void clear() {
-        memTable.clear();
-        sizeCounter = new SizeCounter();
+        try {
+            lock.lock();
+            memTable.clear();
+            sizeBytes.set(0);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Iterator<Entry<OSXMemorySegment>> get(OSXMemorySegment from, OSXMemorySegment to) {
